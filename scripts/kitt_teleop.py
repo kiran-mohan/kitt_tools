cd1bcd6cdde9d724a9fbf6fa8a775f1b2dfd5ebc
Kiran Mohan <kmohan@wpi.edu>
2018-05-20 16:22:34 -0700

    ros-style rospy node

--- scripts/kitt_teleop.py
+++ scripts/kitt_teleop.py
@@ -1,73 +1,80 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 import rospy
-import sys, select, termios, tty
-from readchar import readkey
 from ackermann_msgs.msg import AckermannDrive
+from readchar import readkey
 import numpy as np
 
-def applyLimits(drive_msg, min_speed, max_speed):
-	drive_msg.speed = min(max(drive_msg.speed, min_speed), max_speed)
+def applySpeedLimits(drive_msg, min_speed, max_speed):
+    drive_msg.speed = min(max(drive_msg.speed, min_speed), max_speed)
 
-def main():
-	splash_screen_msg = """
-	Kitt keyboard teleop! Publishing to /platform/drive
-	Increase speed: 'i'
-	Decrease speed: 'k'
-	Turn 10 degrees left: 'j'
-	Turn 10 degrees right: 'l'
-	CTRL-C to quit
-	"""
+def applySteeringLimits(drive_msg, min_steering_angle, max_steering_angle):
+    drive_msg.steering_angle = min(max(drive_msg.steering_angle, min_steering_angle), max_steering_angle)
 
-	# speed increment in m/s
-	speed_increment = 0.1
-	# steering angle increment in radians
-	steering_angle_increment = np.deg2rad(10)
+def teleop():
+    pub = rospy.Publisher('/platform/drive', AckermannDrive, queue_size = 1)
+    rospy.init_node('kitt_teleop')
+    rate = rospy.Rate(100)  # 100Hz
+    splash_screen_msg = """
+        Kitt keyboard teleop! Publishing to /platform/drive
+        Increase speed: 'i'
+        Decrease speed: 'k'
+        Turn 10 degrees left: 'j'
+        Turn 10 degrees right: 'l'
+        CTRL-C to quit
+        """
 
-	bindings = {
-			'i':(speed_increment, 0),
-			'k':(-speed_increment, 0),
-			'j':(0, steering_angle_increment),
-			'l':(0, -steering_angle_increment),
-		       }
+    # speed increment in m/s
+    speed_increment = 0.1
+    # steering angle increment in radians
+    steering_angle_increment = np.deg2rad(10)
 
-	pub = rospy.Publisher('/platform/drive', AckermannDrive, queue_size = 1)
-	rospy.init_node('kitt_teleop')
+    bindings = {
+        'i':(speed_increment, 0),
+        'k':(-speed_increment, 0),
+        'j':(0, steering_angle_increment),
+        'l':(0, -steering_angle_increment),
+    }
 
-	drive = AckermannDrive()
-	steering_offset = rospy.get_param("/drive/steering_offset")
-	min_speed = rospy.get_param("/drive/min_speed")
-	max_speed = rospy.get_param("/drive/max_speed")
-	drive.speed = min_speed
-	drive.steering_angle = steering_offset
-	speed_delta = 0
-	steering_angle_delta = 0
-	print(splash_screen_msg)
-	print("initial:\tspeed %s\tturn %s " % (drive.speed, drive.steering_angle))
-	while(1):
-		key = readkey()
-		# 'r' resets the speed and steering_angle
-		if key == 'r':
-			drive.speed = rospy.get_param("/drive/min_speed")
-			drive.steering_angle = rospy.get_param("/drive/steering_offset")
-			speed_delta = 0
-			steering_angle_delta = 0
-		elif key in bindings.keys():
-			speed_delta = bindings[key][0]
-			steering_angle_delta = bindings[key][1]
-		else:
-			speed_delta = 0
-			steering_angle_delta = 0
-			if (key == '\x03'):
-				drive.speed = rospy.get_param("/drive/min_speed")
-				drive.steering_angle = rospy.get_param("/drive/steering_offset")
-				pub.publish(drive)
-				break
-		drive.speed = drive.speed + speed_delta
-		drive.steering_angle = drive.steering_angle + steering_angle_delta
-		applyLimits(drive, min_speed, max_speed)
-		pub.publish(drive)
+    drive = AckermannDrive()
+    steering_offset = rospy.get_param("/drive/steering_offset")
+    min_speed = rospy.get_param("/drive/min_speed")
+    max_speed = rospy.get_param("/drive/max_speed")
+    min_steering_angle = steering_offset - np.deg2rad(30)
+    max_steering_angle = steering_offset + np.deg2rad(30)
+    drive.speed = min_speed
+    drive.steering_angle = steering_offset
+    speed_delta = 0
+    steering_angle_delta = 0
+    print(splash_screen_msg)
+    while not rospy.is_shutdown():
+        key = readkey()
+        # 'r' resets the speed and steering_angle
+        if key == 'r':
+            drive.speed = rospy.get_param("/drive/min_speed")
+            drive.steering_angle = rospy.get_param("/drive/steering_offset")
+            speed_delta = 0
+            steering_angle_delta = 0
+        elif key in bindings.keys():
+            speed_delta = bindings[key][0]
+            steering_angle_delta = bindings[key][1]
+        else:
+            speed_delta = 0
+            steering_angle_delta = 0
+            if (key == '\x03'):
+                drive.speed = rospy.get_param("/drive/min_speed")
+                drive.steering_angle = rospy.get_param("/drive/steering_offset")
+                pub.publish(drive)
+                break
+        drive.speed = drive.speed + speed_delta
+        drive.steering_angle = drive.steering_angle + steering_angle_delta
+        applySpeedLimits(drive, min_speed, max_speed)
+        applySteeringLimits(drive, min_steering_angle, max_steering_angle)
+        pub.publish(drive)
+        rate.sleep()
 
 if __name__=="__main__":
-	main()
+    try:
+        teleop()
+    except rospy.ROSInterruptException:
+        pass
