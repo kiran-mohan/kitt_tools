d67bd71dc97ec81d5fdd2627571beb4beff24204
Kiran Mohan <kmohan@wpi.edu>
2018-05-20 16:32:46 -0700

    adding comments

--- scripts/teleop.py
+++ scripts/teleop.py
@@ -5,6 +5,17 @@ from ackermann_msgs.msg import AckermannDrive
 from readchar import readkey
 import numpy as np
 
+def printSplashScreenMessage():
+    splash_screen_msg = """
+        Kitt keyboard teleop! Publishing to /platform/drive
+        Increase speed: 'i'
+        Decrease speed: 'k'
+        Turn 10 degrees left: 'j'
+        Turn 10 degrees right: 'l'
+        CTRL-C to quit
+        """
+    print splash_screen_msg
+
 def applySpeedLimits(drive_msg, min_speed, max_speed):
     drive_msg.speed = min(max(drive_msg.speed, min_speed), max_speed)
 
@@ -15,14 +26,6 @@ def teleop():
     pub = rospy.Publisher('/platform/drive', AckermannDrive, queue_size = 1)
     rospy.init_node('kitt_teleop')
     rate = rospy.Rate(100)  # 100Hz
-    splash_screen_msg = """
-        Kitt keyboard teleop! Publishing to /platform/drive
-        Increase speed: 'i'
-        Decrease speed: 'k'
-        Turn 10 degrees left: 'j'
-        Turn 10 degrees right: 'l'
-        CTRL-C to quit
-        """
 
     # speed increment in m/s
     speed_increment = 0.1
@@ -37,16 +40,20 @@ def teleop():
     }
 
     drive = AckermannDrive()
+    # get drive parameters
     steering_offset = rospy.get_param("/drive/steering_offset")
     min_speed = rospy.get_param("/drive/min_speed")
     max_speed = rospy.get_param("/drive/max_speed")
+    # compute min and max steering angles
     min_steering_angle = steering_offset - np.deg2rad(30)
     max_steering_angle = steering_offset + np.deg2rad(30)
+    # initialize drive message
     drive.speed = min_speed
     drive.steering_angle = steering_offset
+    # initialize deltas
     speed_delta = 0
     steering_angle_delta = 0
-    print(splash_screen_msg)
+    printSplashScreenMessage()
     while not rospy.is_shutdown():
         key = readkey()
         # 'r' resets the speed and steering_angle
